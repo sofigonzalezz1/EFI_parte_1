044f39648e30d64c1b4e579c7be4493bbd0f5bf4
Reartes Imanol <imanol_reartes@hotmail.com>
2024-07-27 15:02:16 -0300

    main update

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+#configuracion de SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/repaso_flask_primer_semestre"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Equipo, Modelo, Marca, Fabricante, Pais, Caracteristica, Proveedor, Stock, Accesorio, Persona
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/equipos_list')
+def equipos():
+    return render_template('equipos_list.html')
+
+@app.route('/accesorio_list')
+def accesorios():
+    return render_template('accesorio_list.html')
+
+@app.route('/caracteristica_list')
+def caracteristicas():
+    return render_template('caracteristica_list.html')
+
+@app.route('/fabricante_list')
+def fabricantes():
+    return render_template('fabricante_list.html')
+
+@app.route('/marca_list')
+def marcas():
+    return render_template('marca_list.html')
+
+@app.route('/modelo_list')
+def modelos():
+    return render_template('modelo_list.html')
+
+@app.route('/proveedor_list')
+def proveedores():
+    return render_template('proveedor_list.html')
+
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
+
---
+++ models.py
@@ -0,0 +1,80 @@
+from app import db
+
+""" class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def _str_(self):
+        return self.nombre
+    
+class Tipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def _str_(self):
+        return self.nombre """
+
+class Equipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    modelo_id = db.Column(db.Integer, db.ForeignKey('modelo_id'), nullable=False)
+    categoria = db.Column(db.String(50), nullable=False)
+    costo = db.Column(db.Integer, nullable=False)
+
+class Modelo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    fabricante_id = db.Column(db.Integer, db.ForeignKey('fabricante_id'), nullable=False)
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    categoria = db.Column(db.String(50), nullable=False)
+
+class Fabricante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    pais_id = db.Column(db.Integer, db.ForeignKey('pais_id'), nullable=False)
+
+class Pais(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+class Caracteristica(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    caracteristicas = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(150), nullable=False)
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cantidad_disponible = db.Column(db.Integer, nullable=False)
+    cantidad_minima = db.Column(db.Integer, nullable=False)
+    ubicacion_almacen = db.Column(db.String(100), nullable=False)
+
+class Proveedor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    persona_id = db.Column(db.Integer, db.ForeignKey('persona_id'), nullable=False)
+    contacto = db.Column(db.String(100), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    
+class Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    accesorio = db.Column(db.String(100), nullable=False)
+    """ compatibilidad = db.Column(db.bool) """
+
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    direccion = db.Column(db.String(200), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    correo = db.Column(db.String(100), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+    documento = db.Column(db.String(20), nullable=False)
+    genero = db.Column(db.String(10), nullable=False)
+
+
+
+
+
+
+    
